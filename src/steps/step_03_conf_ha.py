5396165bacdb39a25def08cad075c93f55655ad9
jotape75 <degraus@gmail.com>
2025-09-16 19:58:27 -0300

    update

--- src/steps/step_03_conf_ha.py
+++ src/steps/step_03_conf_ha.py
@@ -21,7 +21,6 @@ import sys
 import os
 import time
 
-from projects.ftd_jenkins_automation.src.utils_ftd import FTD_DEVICES_TEMPLATE
 # Add the src directory to the Python path for imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
